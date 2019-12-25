b733a0c999b595dcf9786e8cda9ee791c4e99948
Hector Serrano <fonto.serrano@gmail.com>
2019-12-24 20:58:46 -0600

    middleware

--- src/app.js
+++ src/app.js
@@ -2,18 +2,33 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.set("views", "views");
+app.set("view engine", "hbs");
+
 // Routes
 app.get("/", (req, res) => {
-	res.sendFile("index.html", {
-		root: path.join(__dirname, "../public")
-	});
+	res.render("index");
 });
 
 app.get("/about", (req, res) => {
-	res.sendFile("about.html", {
-		root: path.join(__dirname, "../public")
-	});
+	res.render("about");
 });
+
+// app.get("/", (req, res) => {
+// 	res.sendFile("index.html", {
+// 		root: path.join(__dirname, "../public")
+// 	});
+// });
+
+// app.get("/about", (req, res) => {
+// 	res.sendFile("about.html", {
+// 		root: path.join(__dirname, "../public")
+// 	});
+// });
 
 // Start listening on a PORT
 app.listen("3000", () => {
